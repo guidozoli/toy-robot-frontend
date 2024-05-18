da4db8bb5cd124261a842c52eeded8884b6e7376
guidozoli <64744713+guidozoli@users.noreply.github.com>
2024-05-18 18:21:30 +0200

    handled move

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,25 +1,27 @@
-import { Component, HostListener, Input } from '@angular/core';
-import { RobotComponent, RotationDirection } from '../robot/robot.component';
+import { Component, HostListener } from '@angular/core';
+import { Command, RobotComponent } from '../robot/robot.component';
 
 @Component({
   selector: 'app-board',
   standalone: true,
   imports: [RobotComponent],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrl: './board.component.scss',
 })
 export class BoardComponent {
-  @Input() rotateTo?: {direction: RotationDirection};
+  command?: Command;
   @HostListener('window:keyup', ['$event']) handleKeyUp(event: KeyboardEvent) {
-    console.log('keyup', event)
-    switch(event.key) {
+    console.log('keyup', event);
+    switch (event.key) {
       case 'ArrowLeft':
-        this.rotateTo = {direction: 'left'}
+        this.command = { type: 'rotate', direction: 'left' };
         break;
       case 'ArrowRight':
-        this.rotateTo = {direction: 'right'}
+        this.command = { type: 'rotate', direction: 'right' };
         break;
+      case 'm':
+      case 'M':
+        this.command = { type: 'move' };
     }
-
   }
 }
--- src/app/robot/robot.component.ts
+++ src/app/robot/robot.component.ts
@@ -1,30 +1,98 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 export type Direction = 'up' | 'right' | 'down' | 'left';
 export type RotationDirection = 'right' | 'left';
 
+export type PlaceCommand = {
+  type: 'place';
+  position: { x: number; y: number };
+};
+export interface MoveCommand {
+  type: 'move';
+}
+export interface RotateCommand {
+  type: 'rotate';
+  direction: RotationDirection;
+}
+
+export interface ReportCommand {
+  type: 'report';
+}
+
+export type Command =
+  | MoveCommand
+  | RotateCommand
+  | PlaceCommand
+  | ReportCommand;
+
 @Component({
   selector: 'app-robot',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgStyle],
   templateUrl: './robot.component.html',
   styleUrl: './robot.component.scss',
 })
 export class RobotComponent implements OnChanges {
-  @Input() rotateTo?: { direction: RotationDirection };
+  @Input() command?: Command;
 
   public direction: Direction = 'down';
+  public position: { x: number; y: number } = { x: 0, y: 0 };
 
   ngOnChanges(changes: SimpleChanges): void {
     if (
-      changes['rotateTo'] &&
-      changes['rotateTo'].currentValue &&
-      !changes['rotateTo'].firstChange
+      changes['command'] &&
+      changes['command'].currentValue &&
+      !changes['command'].firstChange
     ) {
-      this.direction = this.calculateNewDirection(this.rotateTo!.direction);
-      console.log('direction', this.direction)
+      switch (this.command?.type) {
+        case 'rotate':
+          this.handleRotateCommand(this.command);
+          break;
+        case 'move':
+          this.handleMoveCommand(this.command);
+      }
     }
+  }
+
+  getRobotPosition() {
+    return {
+      left: `${this.position.x}px`,
+      bottom: `${this.position.y}px`,
+    };
+  }
+
+  private handleRotateCommand(command: RotateCommand) {
+    this.direction = this.calculateNewDirection(command.direction);
+  }
+
+  private handleMoveCommand(command: MoveCommand) {
+    let x = this.position.x;
+    let y = this.position.y;
+    this.updatePosition();
+  }
+
+  private updatePosition() {
+    let x = this.position.x;
+    let y = this.position.y;
+    switch (this.direction) {
+      case 'left':
+        x = x - 100;
+        break;
+      case 'right':
+        x = x + 100;
+        break;
+      case 'down':
+        y = y - 100;
+        break;
+      case 'up':
+        y = y + 100;
+        break;
+    }
+    this.position = {
+      x,
+      y,
+    };
   }
 
   private rotateLeft(): Direction {
